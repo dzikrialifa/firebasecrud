527254d82d87a0309b0ccca6dbf8b750c1b23138
dzikrialifa <dzikrialif99@gmail.com>
2020-04-30 15:07:30 +0700

    Config firebase

---
+++ src/firebase/Config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDwGUcXEyUaqqn-KdcUn7XcJbSyJ6RwgcE",
+    authDomain: "fir-login-73d25.firebaseapp.com",
+    databaseURL: "https://fir-login-73d25.firebaseio.com",
+    projectId: "fir-login-73d25",
+    storageBucket: "fir-login-73d25.appspot.com",
+    messagingSenderId: "82366059640",
+    appId: "1:82366059640:web:fa2092125fdc3259a8898d",
+    measurementId: "G-ZN1EBZFR8F"
+};
+export default firebaseConfig;
